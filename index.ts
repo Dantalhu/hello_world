eddaa40d520ad416784766597da98cf80dd603d8
dantalhu <maximilyi@yandex.ru>
2025-09-14 12:01:43 +0500

    sum

--- index.ts
+++ index.ts
@@ -1,1 +1,12 @@
-console.log("Hello world")
+
+
+function calc(x: number, y: number): number {
+    let c = x * y;
+    return c;
+}
+
+let result = 1
+for (let index = 1; index <= 50; index++) {
+    result = calc(index,result)
+}
+console.log(result)
